10ddd26a4dd89ee22f4931e5403c06021993f5f9
Manas-20 <manasgotefode19@gmsil.com>
2025-03-28 01:49:59 +0530

    changes

--- src/main/java/com/email/writer/EmailWriterSbApplication.java
+++ src/main/java/com/email/writer/EmailWriterSbApplication.java
@@ -2,8 +2,10 @@ package com.email.writer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.email.writer.app")
 public class EmailWriterSbApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +13,4 @@ public class EmailWriterSbApplication {
 	}
 
 }
+
